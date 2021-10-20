f0d42587c3d7c614381e49c7521ee74f4f784f2f
Juubes <grumpt1@gmail.com>
2021-10-20 17:41:23 +0300

    made cards responsive

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,30 +7,35 @@ const UserProfile = ({ imageUrl, name, description }) => {
   return (
     <div
       style={{
-        maxWidth: "350px",
-        backgroundColor: "gray",
-        borderRadius: "15px"
+        margin: "0 auto"
       }}
     >
-      <div>
-        <img
-          style={{
-            marginBottom: "0.25em",
-            borderRadius: "15px",
-            width: "100%",
-            height: "350px",
-            objectFit: "cover"
-          }}
-          class="m-5 profile-image"
-          alt="profile picture"
-          src={imageUrl}
-        ></img>
-      </div>
-      <div style={{ textAlign: "center" }}>
-        <h2 class="name" style={{ margin: "0.5em 0" }}>
-          {name}
-        </h2>
-        <p class="desc">{description}</p>
+      <div
+        style={{
+          margin: "em",
+          backgroundColor: "#05DBF2",
+          borderRadius: "15px",
+          width: "300px"
+        }}
+      >
+        <div>
+          <img
+            style={{
+              marginBottom: "0.25em",
+              borderRadius: "15px 15px 0 0",
+              width: "100%",
+              height: "350px",
+              objectFit: "cover",
+              marginBottom: "0.5em"
+            }}
+            alt="profile picture"
+            src={imageUrl}
+          ></img>
+        </div>
+        <div style={{ textAlign: "center" }}>
+          <h2 style={{ marginBottom: "0.5em" }}>{name}</h2>
+          <p style={{ marginBottom: "1em" }}>{description}</p>
+        </div>
       </div>
     </div>
   )
