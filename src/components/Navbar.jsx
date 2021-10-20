fde2b3fa5dca2b684c0ea71d29d7fe9e29ba35ff
Koozu1 <onni.pylvanen@gmail.com>
2021-10-20 18:07:31 +0300

    dev

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,3 +5,5 @@ const Navbar = () => {
   )
 
 }
+
+export default Navbar
--- src/pages/testi.jsx
+++ src/pages/testi.jsx
@@ -2,10 +2,42 @@ import React from "react"
 
 
 const TestPage = () => {
+  const style = {
+    backgroundColor: "#083D77",
+    height: "50px",
+  }
+  const gridStyle ={
+    backgroundColor: "red",
+    width: "500px",
+    float: "right",
+    display: "flex",
+    flexDirection: "row-reverse",
+    gap: "20px",
+    margin: "auto",
+  }
+  const buttonStyle = {
+    items: "center",
+    textAlign: "center",
+
+  }
+  const items = [
+    {name: "Eka"},
+    {name: "Toka"},
+    {name: "Kolmas"},
+    {name: "Viides"},
+  ]
+
   return (
-    <div style={{ backgroundColor: "black" }}>
-      <p>Hi from the second page</p>
+    <div style={style}>
+      <div style={gridStyle}>
+        {items.map((item, index) =>(
+          <div style={buttonStyle} key={index}>
+            <p className={[{verticalAlign: "middle"}, {textAlign: "center"}]}>{item.name}</p>
+          </div>
+        ))}
+      </div>
     </div>
+
   )
 }
 
