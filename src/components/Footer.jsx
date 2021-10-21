8148afe2508808ae0a4b325ba35ffd40ed19a29a
Juubes <grumpt1@gmail.com>
2021-10-21 18:24:04 +0300

    WIP

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <footer style={{ height: "150px", backgroundColor: "#083D77" }}>
+      Hello
+    </footer>
+  )
+}
+
+export default Footer
