7263cfcbf9428dc22a3310d9bc9dfff800b22d18
Juubes <grumpt1@gmail.com>
2021-10-21 18:35:33 +0300

    removed gatsby template code

--- src/components/layout.js
+++ src/components/layout.js
@@ -39,9 +39,6 @@ const Layout = ({ children }) => {
             marginTop: `2rem`,
           }}
         >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
         </footer>
       </div>
     </>
