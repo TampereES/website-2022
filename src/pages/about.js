6bf4945e2ccebc430f343747f827a1ecdac68d5e
Juubes <grumpt1@gmail.com>
2021-10-22 21:13:25 +0300

    added about page template

---
+++ src/pages/about.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Layout from "../components/layout"
+
+function AboutPage() {
+  return <Layout>This is the about page template</Layout>
+}
+
+export default AboutPage
