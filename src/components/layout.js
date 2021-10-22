8eb6ece4c4a792ff884a67cfcb24221819e43f2f
Juubes <grumpt1@gmail.com>
2021-10-22 18:31:08 +0300

    added global CSS file

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,9 +8,9 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import './global.css'
 
 import Header from "./header"
-import "./layout.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
