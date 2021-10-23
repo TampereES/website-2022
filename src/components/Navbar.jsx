a3bdd1312c981cfed22d67ed550403b767e91750
Juubes <grumpt1@gmail.com>
2021-10-23 18:03:01 +0300

    moved code to Navbar.js

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,39 @@
-
 const Navbar = () => {
-  return(
+  const gridStyle = {
+    backgroundColor: "red",
+    width: "500px",
+    float: "right",
+    display: "flex",
+    flexDirection: "row-reverse",
+    justifyContent: "space-between",
+    margin: "auto"
+  }
+  const buttonStyle = {
+    items: "center",
+    textAlign: "center"
+  }
 
+  const items = [
+    { name: "About", link: "/about" },
+    { name: "Projects", link: "/projects" },
+    { name: "Contact", link: "/contact" }
+  ]
+
+  return (
+    <div style={style}>
+      <img src={logo} alt="logo" style={{ height: "20%", padding: "5px" }} />
+
+      <div style={gridStyle}>
+        {items.map((item, index) => (
+          <div style={{ verticalAlign: "middle", textAlign: "center" }}>
+            <Link href={item.link} local>
+              {item.name}
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
   )
-
 }
 
 export default Navbar
