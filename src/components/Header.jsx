3d6523e0edc5300f1b137400d1788a38cb4a3467
Juubes <grumpt1@gmail.com>
2021-10-31 16:07:26 +0200

    Added link to header logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,31 +4,26 @@ import logo from "../images/Logo2-Dark.png"
 import Container from "./Container"
 import Navbar from "./Navbar"
 import Image from "next/image"
+import Link from "@/components/Link"
 
 const Header = () => {
   return (
     <header>
       <Container style={{ display: "flex", justifyContent: "center" }}>
-        <Image
-          src={logo}
-          objectFit="contain"
-          height="60px"
-          width="200px"
-          alt="logo"
-          priority
-        />
+        <Link href="/" local>
+          <Image
+            src={logo}
+            objectFit="contain"
+            height="60px"
+            width="200px"
+            alt="logo"
+            priority
+          />
+        </Link>
         <Navbar />
       </Container>
     </header>
   )
-}
-
-Header.propTypes = {
-  siteTitle: PropTypes.string
-}
-
-Header.defaultProps = {
-  siteTitle: ``
 }
 
 export default Header
