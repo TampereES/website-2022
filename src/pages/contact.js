49794cb35e2ae6dd42384fb1ace7554ddda7a4c1
Juubes <grumpt1@gmail.com>
2021-10-31 22:40:17 +0200

    created contact page template

---
+++ src/pages/contact.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+function ContactPage() {
+  return <Layout>This is the contact page template</Layout>
+}
+
+export default ContactPage
