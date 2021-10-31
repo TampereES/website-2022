16669ceb15483fb38c88dc54cba69b9b958d7cb0
Juubes <grumpt1@gmail.com>
2021-10-31 23:45:57 +0200

    WIP added homepage paragraphs

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,25 @@ import Layout from "@/components/Layout"
 
 const IndexPage = () => (
   <Layout>
-    <p> Tervetuloa etusivulle. Work in progress</p>
+    <div style={{ width: "75%" }}>
+      <h1>Encouraging inspired people to take action.</h1>
+      <p>
+        TampereES is a student-run entrepreneurship community that encourages
+        people inspired people to take action.
+      </p>
+    </div>
+
+    <div style={{ width: "75%" }}>
+      <h1>Take action.</h1>
+      <p>
+        TampereES organizes events and other networking opportunities for you to
+        get involved. Come meet some other like-minded people and get to know
+        us.
+      </p>
+    </div>
+
+    <h1 id="events">Upcoming events</h1>
+    <p style={{ color: "green" }}>TODO: carousel of events</p>
   </Layout>
 )
 
