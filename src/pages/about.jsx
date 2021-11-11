1547a895943ba853fc49169dd292959299c23b53
Krister Alasaarela <krister.alasaarela@gmail.com>
2021-11-11 15:16:30 +0200

    WIP updated home page styles

---
+++ src/pages/about.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+import UserProfile from "../components/Card"
+// import Navbar from "src/components/n"
+
+function About() {
+  return (
+    <div className="txt-block">
+      <header>FUG DIS SHID</header>
+
+    </div>
+    
+  )
+}
+
+
+export default About
+export default UserProfile
