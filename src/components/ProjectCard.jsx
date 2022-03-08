802090d17e874e47a8d7ab9c6e44b22dc043967e
Juho Tapio <grumpt1@gmail.com>
2022-03-08 13:35:06 +0200

    proper responsive and centered card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -25,38 +25,32 @@ function ProjectCard({ closeCard, title, description, links }) {
       document.removeEventListener("mousedown", handleClickOutside)
     }
   }, [])
-
+  // pt-10 sm:pt-20 md:pt-40
   return (
-    <div className="top-0 z-30 fixed">
-      <div className="pt-52 w-screen h-screen bg-opacity-75 bg-gray-700">
-        <div className="container mx-auto max-w-4xl bg-white text-dark-blue rounded-lg justify-center">
-          <div ref={ref}>
-            <div className="p-10">
-              <h3 className="text-7xl font-bold mb-10">{title}</h3>
-              <p className="leading-8 text-xl md:leading-9 md:text-3xl">
-                {description}
-              </p>
-            </div>
-            <div className="md:flex justify-around gap-3 bg-dark-blue rounded-b-lg p-4">
-              {links &&
-                links.map((link) => {
-                  return (
-                    <>
-                      <Link
-                        key={link.url}
-                        href={link.url}
-                        className="block py-2"
-                      >
-                        <button>
-                          <span className="m-5 text-white text-xl font-bold overflow-x-hidden">
-                            {link.text}
-                          </span>
-                        </button>
-                      </Link>
-                    </>
-                  )
-                })}
-            </div>
+    <div className="top-0 z-30 fixed flex flex-col justify-center h-screen w-screen bg-opacity-75 bg-gray-700">
+      <div className="flex justify-center mx-5">
+        <div className="my-10 max-w-4xl bg-white text-dark-blue rounded-lg overflow-scroll">
+          <div className="p-10" ref={ref}>
+            <h1 className="font-bold mb-5 text-xl sm:text-5xl lg:text-6xl lg:mb-10">{title}</h1>
+            <p className="text-sm sm:text-2xl md:leading-9">
+              {description}
+            </p>
+          </div>
+          <div className="md:flex justify-around md:gap-3 bg-dark-blue rounded-b-lg p-4">
+            {links &&
+              links.map((link) => {
+                return (
+                  <>
+                    <Link key={link.url} href={link.url} className="block py-2">
+                      <button>
+                        <span className="m-5 text-white text-xl font-bold overflow-x-hidden">
+                          {link.text}
+                        </span>
+                      </button>
+                    </Link>
+                  </>
+                )
+              })}
           </div>
         </div>
       </div>
